c88c69f83a7c384dba516c6999919dccf824d959
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-02 22:01:09 -0300

    check the exam

---
+++ check-the-exam.js
@@ -0,0 +1,30 @@
+// The first input array is the key to the correct answers to an exam,
+// like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array
+// of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each
+// blank answer, represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+const assert = require("assert");
+
+// Código
+const checkExam = (array1, array2) => {
+  let grade = 0;
+  array2.forEach((element, index, array) => {
+    array2[index].length === 0 ? (grade += 0)
+      : element === array1[index] ? (grade += 4)
+      : element !== array1[index] ? (grade -= 1)
+      : grade;
+  });
+  grade < 0 ? grade = 0 : grade;
+  return grade;
+};
+
+
+// Unit test
+assert.deepStrictEqual(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]), 6);
+assert.deepStrictEqual(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]), 7);
+assert.deepStrictEqual(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]), 16);
+assert.deepStrictEqual(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]), 0);
