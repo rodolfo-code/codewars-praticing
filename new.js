a9303d28af972b87e812880fa7982e45c2987e92
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-09 05:19:34 -0300

    find operation type

---
+++ new.js
@@ -0,0 +1,20 @@
+// You have to create a function calcType, which receives 3 arguments: 2 numbers, 
+// and the result of an unknown operation performed on them (also a number).
+
+// Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+
+// The possible return strings are: "addition", "subtraction", "multiplication", "division".
+
+const assert = require('assert');
+
+// Código
+const calcType = (a, b, res) =>
+  res === a + b ? 'addition' : 
+  res === a * b ? 'multiplication' : 
+  res === a - b ? 'subtraction' : 'division';
+
+// Unit Test
+assert.deepStrictEqual(calcType(1,2,3),'addition');
+assert.deepStrictEqual(calcType(10,4,40),'multiplication');
+assert.deepStrictEqual(calcType(10,5,5),'subtraction');
+assert.deepStrictEqual(calcType(9,5,1.8),'division');
