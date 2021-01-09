fb3aba766f82bfbd1eb9ca348fa63462ecc1afc0
rodolfo_oliveira <rod.vgo@gmail.com>
2021-01-09 05:48:49 -0300

    sum of digits

---
+++ sumOfDigits.js
@@ -0,0 +1,19 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, 
+// continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+const assert = require('assert');
+
+// Código
+const digital_root = (n) => {
+  let cont = 
+  if(n.length > 1) {
+    n.split('')
+  }
+
+}
+
+console.log(digital_root(123))
+
+// Unit test
